45f4ec6b8ff9a724c7514b39693dd4d172af3c67
Aravind Sundaresan <asundaresan@gmail.com>
2017-02-10 08:56:16 -0800

    First commit

---
+++ external/thing/src/thing.cpp
@@ -0,0 +1,6 @@
+#include <thing.h>
+#include <iostream>
+
+int do_thing( ) {
+  std::cout << "Thing 1" << std::endl;
+}
---
+++ src/myapp.cpp
@@ -0,0 +1,5 @@
+#include "thing.h"
+
+int main( int argc, char *argv[] ) {
+  do_thing();
+}
